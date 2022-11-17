8ae572ed51ba04c219e7b35fa790497bc969d0c7
Artyom Untila <untila_artem@mail.ru>
2022-11-17 17:44:14 +0300

    first

--- src/main/kotlin/org/vorpal/research/kex/plugin/ProjectViewAction.kt
+++ src/main/kotlin/org/vorpal/research/kex/plugin/ProjectViewAction.kt
@@ -4,12 +4,22 @@ import com.intellij.execution.filters.TextConsoleBuilderFactory
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager
+import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessModuleDir
+import com.intellij.openapi.project.rootManager
 import com.intellij.openapi.roots.OrderEnumerator
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.task.ProjectTaskManager
+import org.jetbrains.jps.model.java.JavaSourceRootType
+import org.jetbrains.kotlin.idea.configuration.isGradleModule
+import org.jetbrains.kotlin.idea.maven.isMavenModule
+import org.jetbrains.kotlin.idea.util.findModule
+import org.jetbrains.kotlin.idea.util.projectStructure.getModule
+import org.jetbrains.kotlin.idea.util.sourceRoots
 import org.vorpal.research.kex.plugin.util.*
 
 class ProjectViewAction : AnAction() {
@@ -17,18 +27,49 @@ class ProjectViewAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
 
-        val psiFile = e.getData(CommonDataKeys.PSI_FILE) ?: return
-        val target = getPsiFileFQN(psiFile)
-        val classpathList = getClasspathList(project)
+//        val psiFile = e.getData(CommonDataKeys.PSI_FILE) ?: return
+//        val target = getPsiFileFQN(psiFile)
+//        val classpathList = getClasspathList(project)
+//
+//        val projectTaskManager = ProjectTaskManager.getInstance(project)
+//        // There is an approach to get module dependencies and build it separately, but it is much more complicated (it will also affect the way classpath is obtained)
+//        val buildResult = projectTaskManager.buildAllModules()
+//        val modules = project.allModules()
+//        val additionalOptions = CompilerConfiguration.getInstance(project).getAdditionalOptions(modules.first())
 
-        val projectTaskManager = ProjectTaskManager.getInstance(e.project)
-        // There is an approach to get module dependencies and build it separately, but it is much more complicated (it will also affect the way classpath is obtained)
-        val buildResult = projectTaskManager.buildAllModules()
+//        buildResult.onSuccess {
+//            val dockerKexArgsList = getDockerKexArgsList(classpathList, "", target)
+//            Messages.showInfoMessage(project, dockerKexArgsList.joinToString("\n"), "Command")
+//            Messages.showInfoMessage(project, modules.joinToString("\n"), "All Modules")
+//            //launchKex(project, classpathList, "", target)
+//        }
+
+        val psiFile = e.getData(CommonDataKeys.PSI_FILE) ?: return
+        val module = ModuleUtil.findModuleForFile(psiFile) ?: return
+
+        val projectTaskManager = ProjectTaskManager.getInstance(project)
+        val buildResult = projectTaskManager.build(module)
+
+        val buildSystem = ExternalSystemModulePropertyManager.getInstance(module).getExternalSystemId()?.uppercase()
+        val moduleVirtualFile = module.guessModuleDir()
+        val modulePath = moduleVirtualFile?.canonicalPath
+        val sourceRoots = module.sourceRoots.map { it.path }
+        var testRoots = module.rootManager.getSourceRoots(JavaSourceRootType.TEST_SOURCE).joinToString("\n") { it.path }
+
+        if (module.isGradleModule()) {
+            val testVirtualFile = moduleVirtualFile?.parent?.children?.find { it.name == "test" }
+            val testModule = testVirtualFile!!.findModule(project)!!
+            testRoots = testModule.rootManager.getSourceRoots(JavaSourceRootType.TEST_SOURCE).joinToString("\n") { it.path }
+        }
 
         buildResult.onSuccess {
-            val dockerKexArgsList = getDockerKexArgsList(classpathList, "", target)
-            Messages.showInfoMessage(project, dockerKexArgsList.joinToString("\n"), "Command")
-            launchKex(project, classpathList, "", target)
+            Messages.showInfoMessage(project, "Module [${module.name}] is successfully built", "Build Result")
+            Messages.showInfoMessage(project, buildSystem, "External System Id")
+            Messages.showInfoMessage(project, "${module.isMavenModule()}", "Is Maven?")
+            Messages.showInfoMessage(project, "${module.isGradleModule()}", "Is Gradle?")
+            Messages.showInfoMessage(project, modulePath, "Module Dir Path")
+            Messages.showInfoMessage(project, sourceRoots.joinToString("\n"), "Module Source Roots")
+            Messages.showInfoMessage(project, testRoots, "Module Test Roots")
         }
     }
 
