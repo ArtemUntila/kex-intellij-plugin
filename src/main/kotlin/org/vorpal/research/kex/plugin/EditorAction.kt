44e8d75a0882eafe5a22d5ce51ce043c4431001f
Artyom Untila <untila_artem@mail.ru>
2023-03-18 17:36:45 +0300

    Correct EditorActionGroup trigger

--- src/main/kotlin/org/vorpal/research/kex/plugin/EditorAction.kt
+++ src/main/kotlin/org/vorpal/research/kex/plugin/EditorAction.kt
@@ -1,6 +1,7 @@
 package org.vorpal.research.kex.plugin
 
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.module.ModuleUtil
 import org.vorpal.research.kex.plugin.util.psi.targetName
 
@@ -8,7 +9,7 @@ class EditorAction : KexBaseAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
-        val psiElement = EditorActionGroup.psiElement
+        val psiElement = e.getData(CommonDataKeys.PSI_ELEMENT) ?: return
         val module = ModuleUtil.findModuleForPsiElement(psiElement) ?: return
         val target = psiElement.targetName ?: return
 
--- src/main/kotlin/org/vorpal/research/kex/plugin/EditorActionGroup.kt
+++ src/main/kotlin/org/vorpal/research/kex/plugin/EditorActionGroup.kt
@@ -4,26 +4,27 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.util.elementType
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtFunction
 
 class EditorActionGroup : DefaultActionGroup() {
 
-    companion object {
-        lateinit var psiElement: PsiElement
-    }
-
     override fun update(e: AnActionEvent) {
         val psiFile = e.getData(CommonDataKeys.PSI_FILE) ?: return
         val editor = e.getData(CommonDataKeys.EDITOR) ?: return
-        val psiElement = psiFile.viewProvider.findElementAt(editor.caretModel.offset)?.parent
-        if (psiElement != null) {
-            val isJavaElement = psiElement is PsiClass || psiElement is PsiMethod
-            val isKotlinElement = psiElement is KtClass || psiElement is KtFunction
-            e.presentation.isEnabledAndVisible = isJavaElement || isKotlinElement
-            EditorActionGroup.psiElement = psiElement
-        }
+
+        val psiElement = psiFile.viewProvider.findElementAt(editor.caretModel.offset)
+        val psiElementParent = psiElement?.parent
+
+        if (psiElement == null || psiElementParent == null) return
+
+        // Couldn't come up with something more elegant
+        val isIdentifierElement = psiElement.elementType?.debugName == "IDENTIFIER"
+        val isJavaParent = psiElementParent is PsiClass || psiElementParent is PsiMethod
+        val isKotlinParent = psiElementParent is KtClass || psiElementParent is KtFunction
+
+        e.presentation.isEnabledAndVisible = isIdentifierElement && (isJavaParent || isKotlinParent)
     }
 }
