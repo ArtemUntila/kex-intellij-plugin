44d975dafdba349a8a2be8bbbf6356da8f20b299
Artyom Untila <untila_artem@mail.ru>
2023-05-15 01:28:19 +0300

    runner refactoring

--- src/main/kotlin/org/vorpal/research/kex/plugin/actions/EditorAction.kt
+++ src/main/kotlin/org/vorpal/research/kex/plugin/actions/EditorAction.kt
@@ -4,17 +4,17 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.module.ModuleUtil
-import org.vorpal.research.kex.plugin.runner.KexRunner
-
 import org.vorpal.research.kex.plugin.psi.targetName
+import org.vorpal.research.kex.plugin.runner.KexRunner
 
 class EditorAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
         val psiElement = e.getData(CommonDataKeys.PSI_ELEMENT) ?: return
         val module = ModuleUtil.findModuleForPsiElement(psiElement) ?: return
         val target = psiElement.targetName ?: return
 
-        KexRunner(module).buildAndRun(target, true)
+        KexRunner(project).buildAndRun(module, target, true)
     }
 }
--- src/main/kotlin/org/vorpal/research/kex/plugin/actions/ProjectViewAction.kt
+++ src/main/kotlin/org/vorpal/research/kex/plugin/actions/ProjectViewAction.kt
@@ -4,16 +4,17 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.module.ModuleUtil
-import org.vorpal.research.kex.plugin.runner.KexRunner
 import org.vorpal.research.kex.plugin.psi.fileClassFqName
+import org.vorpal.research.kex.plugin.runner.KexRunner
 
 class ProjectViewAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
         val psiFile = e.getData(CommonDataKeys.PSI_FILE) ?: return
         val module = ModuleUtil.findModuleForFile(psiFile) ?: return
         val target = psiFile.fileClassFqName?.asString() ?: return
 
-        KexRunner(module).buildAndRun(target, true)
+        KexRunner(project).buildAndRun(module, target, true)
     }
 }
